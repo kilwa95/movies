877ef38b34d77b1ae93775ed8a711bf9f9d53a78
kilwa95 <lga.sig.khaled@gmail.com>
2020-07-15 14:54:54 +0200

    Mise en place du routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import { Header } from "./components";
 import apiMovie, { apiMovieMap }  from './components/conf/api.movie';
-import Films from './features/filmes'
+import Films from './features/filmes';
+import Favoris from './features/favories';
+import { BrowserRouter as Router,Route ,Switch ,Redirect} from "react-router-dom";
 
 
 class App extends Component {
@@ -42,16 +44,27 @@ class App extends Component {
 
   render() {
     return (
+      <Router>
       <div className="App d-flex flex-column">
         <Header />
-        <Films 
-        loaded = {this.state.loaded}
-        updateMovies = {this.updateMovies}
-        updateSelectedMovie = {this.updateSelectedMovie}
-        movies = {this.state.movies}
-        selectedMovie = {this.state.selectedMovie}
-        />
+        <Switch>
+          <Route path="/films" render={ (props) => {
+            return(
+              <Films 
+              { ...props } 
+              loaded = {this.state.loaded}
+              updateMovies = {this.updateMovies}
+              updateSelectedMovie = {this.updateSelectedMovie}
+              movies = {this.state.movies}
+              selectedMovie = {this.state.selectedMovie}
+              />
+            )
+          }} />
+          <Route path="/favoris"  component={ Favoris}/>
+          <Redirect to="/films" />
+        </Switch>
       </div>
+      </Router>
     );
   }
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {NavLink} from 'react-router-dom';
 
 export default class Header extends Component {
   render() {
@@ -11,10 +12,10 @@ export default class Header extends Component {
         <div className="collapse navbar-collapse">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="/">Home</a>
+              <NavLink to="/films" className="nav-link">Home</NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">Favoris</a>
+            <NavLink to="/favoris" className="nav-link">Favoris</NavLink>
             </li>
           </ul>
         </div>
